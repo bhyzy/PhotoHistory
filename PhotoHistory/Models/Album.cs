0091103d337e4cbe8e3fc149ed0d547ebb06455c
unknown <Qba@Qba-laptop.(none)>
2012-04-23 01:31:07 +0200

    album model

---
+++ PhotoHistory/Models/Album.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoHistory.Models
+{
+    public class Album
+    {
+        public int? Id {get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public DateTime? NextNotification { get; set; }
+        public bool Public { get; set; }
+        public string Password { get; set; }
+        public bool CommentsAllow { get; set; }
+        public bool CommentsAuth { get; set; }
+
+    }
+}
