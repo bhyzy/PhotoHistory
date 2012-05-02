69204b85ff259bec7739cc55ed9d1620a5035201
Michal <pierogmichal@gmail.com>
2012-05-02 16:21:36 +0200

    Przygotowania do uploadu zdjec

---
+++ PhotoHistory/Common/FileHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PhotoHistory.Models;
+
+namespace PhotoHistory.Common
+{
+    public class FileHelper
+    {
+        public void createUserDirectory(UserModel model)
+        {
+        }
+
+        public void createAlbumDirectory(AlbumModel model)
+        { 
+        
+        }
+
+        public void savePhoto()
+        {
+
+        }
+
+        public string getPhoto()
+        {
+            return null;
+        }
+
+        public IEnumerable<string> getAlbumThumbail()
+        {
+            return null;
+        }
+    }
+}
