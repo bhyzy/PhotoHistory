9c83043883cbdaf89cd74005e9d5f6291deb5669
Bartlomiej Hyzy <empitness@gmail.com>
2012-06-04 23:08:41 +0200

    Android: popup potwierdzajacy chec wylogowania

--- PastExplorer_Android/src/com/pastexplorer/DashboardActivity.java
+++ PastExplorer_Android/src/com/pastexplorer/DashboardActivity.java
@@ -1,6 +1,8 @@
 package com.pastexplorer;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class DashboardActivity extends Activity {
@@ -10,4 +12,21 @@ public class DashboardActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
     }
+    
+    @Override
+    public void onBackPressed() {  	
+    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	builder
+    	.setTitle(R.string.confirmSignOutTitle)
+    	.setMessage(R.string.confirmSignOutMessage)
+    	.setIcon(android.R.drawable.ic_dialog_alert)
+    	.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+    	    public void onClick(DialogInterface dialog, int which) {			      	
+    	    	DashboardActivity.super.onBackPressed();
+    	    }
+    	})
+    	.setNegativeButton(android.R.string.no, null)
+    	.show();
+    }
+
 }
