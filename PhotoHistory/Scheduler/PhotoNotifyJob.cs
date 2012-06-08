cd533910fc6236b96b399f059a4ac7cf595feaad
Michal <pierogmichal@gmail.com>
2012-06-08 23:39:06 +0200

    Dodanie Quartza

---
+++ PhotoHistory/Scheduler/PhotoNotifyJob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+
+namespace PhotoHistory.Scheduler
+{
+    public class PhotoNotifyJob : IJob
+    {
+        public PhotoNotifyJob() { }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            System.Diagnostics.Debug.WriteLine("Executing");
+        }
+    }
+
+
+}
