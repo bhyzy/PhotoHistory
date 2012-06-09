bf5c93874b02047d8ed4df519b5dc1ca68a4cf1b
Bartlomiej Hyzy <empitness@gmail.com>
2012-06-09 16:44:41 +0200

    Android: podglad zdjecia - poczatek

--- PastExplorer_Android/src/com/pastexplorer/TakePhotoActivity.java
+++ PastExplorer_Android/src/com/pastexplorer/TakePhotoActivity.java
@@ -1,13 +1,145 @@
 package com.pastexplorer;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class TakePhotoActivity extends Activity {
-    /** Called when the activity is first created. */
+public class TakePhotoActivity extends Activity {	
+	private static final String DEBUG_TAG = "PE TakePhoto";
+	
+	private PowerManager.WakeLock mWakeLock;
+	
+   private PhotoPreview mPreview;
+    Camera mCamera;
+    int numberOfCameras;
+    int cameraCurrentlyLocked;
+
+    // The first rear facing camera
+    int defaultCameraId;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Create a RelativeLayout container that will hold a SurfaceView,
+        // and set it as the content of our activity.
+        //mPreview = new Preview(this);
         setContentView(R.layout.take_photo);
+        mPreview = (PhotoPreview)findViewById(R.id.preview);
+        
+        ImageView overlay = (ImageView)findViewById(R.id.overlay);
+        if (overlay != null) {
+        	overlay.setAlpha(50);
+        }
+
+        // Find the total number of cameras available
+        numberOfCameras = Camera.getNumberOfCameras();
+        Log.d(DEBUG_TAG, "numberOfCameras = " + numberOfCameras);
+
+        // Find the ID of the default camera
+        CameraInfo cameraInfo = new CameraInfo();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
+                defaultCameraId = i;
+                Log.d(DEBUG_TAG, "defaultCameraId = " + defaultCameraId);
+                break;
+            }
+        }
+        
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        mWakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "DoNotDimScreen");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Open the default i.e. the first rear facing camera.
+        mCamera = Camera.open();
+        cameraCurrentlyLocked = defaultCameraId;
+        mPreview.setCamera(mCamera);
+        
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        mWakeLock.acquire();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Because the Camera object is a shared resource, it's very
+        // important to release it when the activity is paused.
+        if (mCamera != null) {
+            mPreview.setCamera(null);
+            mCamera.release();
+            mCamera = null;
+        }
+        
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        mWakeLock.release();
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        // Inflate our menu which can gather user input for switching camera
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.camera_menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle item selection
+//        switch (item.getItemId()) {
+//        case R.id.switch_cam:
+//            // check for availability of multiple cameras
+//            if (numberOfCameras == 1) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                builder.setMessage(this.getString(R.string.camera_alert))
+//                       .setNeutralButton("Close", null);
+//                AlertDialog alert = builder.create();
+//                alert.show();
+//                return true;
+//            }
+//
+//            // OK, we have multiple cameras.
+//            // Release this camera -> cameraCurrentlyLocked
+//            if (mCamera != null) {
+//                mCamera.stopPreview();
+//                mPreview.setCamera(null);
+//                mCamera.release();
+//                mCamera = null;
+//            }
+//
+//            // Acquire the next camera and request Preview to reconfigure
+//            // parameters.
+//            mCamera = Camera
+//                    .open((cameraCurrentlyLocked + 1) % numberOfCameras);
+//            cameraCurrentlyLocked = (cameraCurrentlyLocked + 1)
+//                    % numberOfCameras;
+//            mPreview.switchCamera(mCamera);
+//
+//            // Start the preview
+//            mCamera.startPreview();
+//            return true;
+//        default:
+//            return super.onOptionsItemSelected(item);
+//        }
+//    }
+	
+
 }
